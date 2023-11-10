95b6e7ca468bc8fd617a7b6f5339657469ddd65f
Tejiri Makele <tejirimakele@yahoo.com>
2023-11-10 17:12:48 +0100

    new

--- assets/js/main.js
+++ assets/js/main.js
@@ -202,6 +202,7 @@ num = setInterval(updated, speed);
     var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
     var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
 
+    
     // Change the icons inside the button based on previous settings
     if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
         themeToggleLightIcon.classList.remove('hidden');
